a291bccd20ac311f0d617fe2dc57370a18a50eec
Raj <rajeev.v.singh@gmail.com>
2017-08-28 13:54:29 -0400

    remove babel minify webpack plugin - it broke inline console

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,7 @@
 const merge = require('webpack-merge');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   plugins: [
-    new MinifyPlugin()
   ]
 });
